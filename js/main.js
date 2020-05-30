b4cabd69711dd7cd62cd3d2b7d8dc7e5b2cd2adc
RobertoHp <robertohp0109@gmail.com>
2020-05-29 20:02:50 -0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const NAV = document.querySelector('NAV');
+const ITEM = document.querySelectorAll('nav ul li');
+const HBUTTON = document.querySelector('#menuHamburger');
+
+HBUTTON.onclick = function() {
+    this.classList.toggle('trans');
+    NAV.classList.toggle('show');
+};
+
+function actionMenu() {
+    NAV.classList.remove('show');
+    HBUTTON.classList.remove('trans');
+}
+
+ITEM[0].onclick = actionMenu;
+ITEM[1].onclick = actionMenu;
+ITEM[2].onclick = actionMenu;
+ITEM[3].onclick = actionMenu;
+ITEM[4].onclick = actionMenu;
+
+const d = document;
+const displayClock = d.querySelector('.display-clock');
+const sButton = d.querySelector('.start-clock');
+const dButton = d.querySelector('.stop-clock');
+const sAlarm = d.querySelector('.start-alarm');
+const dAlarm = d.querySelector('.stop-alarm');
+let audioElement = new Audio('../sound/alarma.mp3');
+let t;
+
+function displayTime() {
+    let fecha = new Date();
+    displayClock.innerHTML = fecha.toLocaleTimeString();
+}
+
+sButton.onclick = function() {
+    t = setInterval(displayTime, 1000);
+    sButton.disabled = true;
+    sButton.style.background = 'rgb(120, 92, 146)';
+}
+
+dButton.onclick = function() {
+    clearInterval(t);
+    displayClock.innerHTML = '';
+    sButton.disabled = false;
+    sButton.style.background = 'rgb(87, 27, 143)';
+}
+
+sAlarm.onclick = () => {
+    audioElement.play();
+    sAlarm.disabled = true;
+    sAlarm.style.background = 'rgb(120, 92, 146)';
+}
+
+dAlarm.onclick = () => {
+    audioElement.load();
+    sAlarm.disabled = false;
+    sAlarm.style.background = 'rgb(87, 27, 143)';
+
+}
